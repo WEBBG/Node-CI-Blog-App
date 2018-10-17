d49865ebbd530d89645ffafa1cb2a7539a4c9453
Bruno Garcia <Bruno.Garcia@criticaltechworks.com>
2018-10-17 15:30:33 +0100

    travis config

---
+++ README.md
@@ -0,0 +1,4 @@
+# Blog App
+
+Node.js, React, MongoDB, Google Oauth for login,
+Redis for cache,
---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://bfgarcia986:Z3vVVhEAjEaCJ9A@ds161833.mlab.com:61833/blog_everyone',
+  // mongoURI: 'mongodb://localhost/blog_everyone',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
